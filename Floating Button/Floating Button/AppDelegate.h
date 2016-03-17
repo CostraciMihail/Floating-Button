181c12bbe885084434ad8f238b8c443a502c041d
Costraci Mihail <mihaicostraci3@gmail.com>
2016-03-17 11:00:50 +0200

    First commit

---
+++ Floating Button/Floating Button/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Floating Button
+//
+//  Created by winify on 3/17/16.
+//  Copyright © 2016 Costraci Mihail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Floating Button/Floating Button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Floating Button
+//
+//  Created by winify on 3/17/16.
+//  Copyright © 2016 Costraci Mihail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
